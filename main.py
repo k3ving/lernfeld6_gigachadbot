788813a778567bde9ba249b607f39029616b2686
k3ving <104917909+k3ving@users.noreply.github.com>
2023-06-05 10:40:48 +0000

    clean main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import dictionaries
 from tkinter import *
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 76f86dbdea136cf1601c76f7814e626f189a9483
 def chat():
     root = Tk()
     root.title("Chatbot")
 
-<<<<<<< HEAD
     frame = Frame(master=root, width=750, height=600)
     frame.pack()
-=======
+
     for x in dicts:
         print(x)
->>>>>>> 76f86dbdea136cf1601c76f7814e626f189a9483
 
     start_options = dictionaries.answers["Start"].get_options()
 
@@ -37,9 +31,8 @@ def show_options(key, root):
         button.pack()
 
 
-<<<<<<< HEAD
 chat()
-=======
+
 frame = Tk.frame(master=root, width=500, height=500, bg='gray')
 frame.pack()
 
@@ -48,6 +41,3 @@ w = Label(root, text="Hello bois")
 w.pack()
 
 root.mainloop()
-
-# chat()
->>>>>>> 76f86dbdea136cf1601c76f7814e626f189a9483
