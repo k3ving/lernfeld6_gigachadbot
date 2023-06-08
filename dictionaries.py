c7ea2036fb3a7f7b0c1db6c845c45b8d88c05866
xDoubleVx <135117826+xDoubleVx@users.noreply.github.com>
2023-06-08 10:55:26 +0200

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,7 +1,7 @@
 import answer as ans
 
 answers = {
-    "Start": ans.Answer("Womit kann ich behilflich sein?", ["Absturz", "Problem bei Login"]),
+    "Start": ans.Answer("Hallo ich bin der Chatbot. Mit welchem Problem kann ich Ihnen weiterhelfen?", ["Software", "Hardware", "Netzwerk", "Datensicherheit", "Authnetifikation"]),
     "Absturz": ans.Answer("Ein Absturz. Welche der folgenden Dinge trifft zu?", ["Programm friert ein", "Lädt endlos", "Programm schließt sich"]),
     "Problem bei Login": ans.Answer("Probleme beim Login. Was trifft am ehesten zu?", ["Falscher Nutzername", "Falsche Email", "Falsches Passwort"]),
     "Falscher Nutzername": ans.Answer("Kontaktieren Sie bitte unser Support Team.", ["Danke!"]),
