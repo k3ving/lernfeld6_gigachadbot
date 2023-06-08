50e2ce013cd34c954f0a3933b26a6a6253afbec6
xDoubleVx <135117826+xDoubleVx@users.noreply.github.com>
2023-06-08 12:31:32 +0200

    Update dictionaries.py
    Deleted 2nd copy of dataset

--- dictionaries.py
+++ dictionaries.py
@@ -18,13 +18,4 @@ answers = {
     "Datensicherheit": Answer("Ein Datensicherheit Problem? Welche der folgenden Dinge trifft zu?", ["Hacking Probleme", "Firewall Detection", "Anderes"]),
     "Authentifikation": Answer("Ein Authentifikation Problem? Welche der folgenden Dinge trifft zu?", ["Passwort vergessen", "Anderes"])
     "Anderes": Answer("Ich rate Ihnen sich an unser Support-Team zu wenden, bitte wählen sie eine der Kontaktoptionen.", ["Zum Mitarbeiter weiterleiten", "Gespräch abbrechen", "Ticket erstellen"]),
-    "Programm abgestürzt": Answer("Haben Sie versucht das Programm oder Ihren Rechner neuzustarten?" , ["Funktioniert wieder", "Programm hängt immer noch"])
-    "Update Problem": Answer("Haben Sie versucht das Programm oder Ihren Rechner neuzustarten?" , ["Funktioniert wieder", "Update hängt immer noch"])
-    "Bug": Answer("Haben Sie versucht das Programm oder Ihren Rechner neuzustarten?" , ["Funktioniert wieder", "Bug bleibt bestehen"])
-    "Programm ist langsam": Answer("Haben Sie versucht das Programm oder Ihren Rechner neuzustarten?" , ["Funktioniert wieder", "Programm bleibt langsam"])
-    "Programm bleibt langsam": Answer("Liegt ein Update für das Programm vor?" , ["Funktioniert wieder", "Funktioniert nicht"])
-    "Programm hängt immer noch": Answer("Ich rate Ihnen das Sie unser Support Team kontaktieren." , ["Zum Mitarbeiter weiterleiten", "Gespräch abbrechen", "Ticket erstellen"])
-    "Update hängt immer noch": Answer("Ich rate Ihnen das Sie unser Support Team kontaktieren." , ["Zum Mitarbeiter weiterleiten", "Gespräch abbrechen", "Ticket erstellen"])
-    "Bug bleibt bestehen": Answer("Ich rate Ihnen das Sie unser Support Team kontaktieren." , ["Zum Mitarbeiter weiterleiten", "Gespräch abbrechen", "Ticket erstellen"])
-    "Funktioniert nicht": Answer("Ich rate Ihnen das Sie unser Support Team kontaktieren." , ["Zum Mitarbeiter weiterleiten", "Gespräch abbrechen", "Ticket erstellen"])
 }
