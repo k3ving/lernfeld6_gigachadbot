61fb961753721bbf5ab4a4a0f5ea2d6221365869
Alexander Schmer <schmer@take-e-way.de>
2023-06-08 13:29:31 +0200

    updated database.py

--- database.py
+++ database.py
@@ -16,6 +16,7 @@ class Database:
 
         cursor.execute("SELECT * FROM [dbo].[Chat] WHERE [Id] = ?", id)
         for x in cursor.fetchall():
+            print(x)
             chat_history = x.ChatHistory
             split_chat_history = chat_history.split(';')
 
@@ -23,8 +24,10 @@ class Database:
                 print(message)
 
     def write(self, ticket):
+        print(ticket.CustomerId, ticket.ChatHistory)
         cursor = self.connection.cursor()
-        cursor.execute("INSERT INTO [dbo].[Chat] ([CustomerId, ChatHistory]) VALUES (?, ?)", ticket.CustomerId, ticket.ChatHistory)
+        cursor.execute("INSERT INTO [dbo].[Chat] ([CustomerId], [ChatHistory]) VALUES (?, ?)", ticket.CustomerId, ticket.ChatHistory)
+        cursor.commit()
 
 
 class Ticket:
